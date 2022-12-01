b3c2dcee403f07b46eed8fa45d1bd1a84ebd6b29
Endre Deak <deak@csdisco.com>
2022-11-30 21:57:24 -0800

    Day 01

--- src/main/kotlin/endredeak/aoc2022/Day01.kt
+++ src/main/kotlin/endredeak/aoc2022/Day01.kt
@@ -1,17 +1,22 @@
 package endredeak.aoc2022
 
+import endredeak.aoc2022.lib.utils.splitBy
+
 fun main() {
-    solve("") {
-        mapper {
-            it
+    solve("Calorie Counting") {
+        val input = lines
+            .splitBy { it.isEmpty() }
+            .map { it.sumOf(String::toInt) }
+
+        part1(72478) {
+            input.max()
         }
 
-        part1 { input ->
-
-        }
-
-        part2 { input ->
-
+        part2(210367) {
+            input
+                .sortedDescending()
+                .take(3)
+                .sum()
         }
     }
 }
