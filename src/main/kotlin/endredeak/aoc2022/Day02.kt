854e70af8d4bf428f96c689b0c74614262f3b41f
Endre Deak <deak@csdisco.com>
2022-12-02 08:23:28 -0800

    Day 02

--- src/main/kotlin/endredeak/aoc2022/Day02.kt
+++ src/main/kotlin/endredeak/aoc2022/Day02.kt
@@ -1,15 +1,24 @@
 package endredeak.aoc2022
 
 fun main() {
-    solve( "") {
-        val input = lines
+    solve("Rock Paper Scissors") {
+        val input = lines.map { it.replace(" ", "") }
 
-        part1 {
+        val rounds = listOf("AX", "AY", "AZ", "BX", "BY", "BZ", "CX", "CY", "CZ")
+        val points = listOf(4, 8, 3, 1, 5, 9, 7, 2, 6)
 
+        part1(14069) {
+            input.sumOf { points[rounds.indexOf(it)] }
         }
 
-        part2 {
-
+        part2(12411) {
+            input.sumOf {
+                when {
+                    it.startsWith("A") -> points[(rounds.indexOf(it) + 2) % 3]
+                    it.startsWith("C") -> points[(rounds.indexOf(it) + 1) % 3 + 6]
+                    else -> points[rounds.indexOf(it)]
+                }
+            }
         }
     }
 }
