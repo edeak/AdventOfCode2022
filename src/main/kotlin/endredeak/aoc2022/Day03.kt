4c4d7101619037e4bbc7f752e23d5d5f8a2a1148
Endre Deak <deak@csdisco.com>
2022-12-03 19:56:37 -0800

    Day 03

--- src/main/kotlin/endredeak/aoc2022/Day03.kt
+++ src/main/kotlin/endredeak/aoc2022/Day03.kt
@@ -1,15 +1,24 @@
 package endredeak.aoc2022
 
 fun main() {
-    solve( "") {
-        val input = lines
-
-        part1 {
-
+    solve( "Rucksack Reorganization") {
+        fun Set<Char>.priority() = this.first().let { c ->
+            ('a'..'z').indexOf(c.lowercaseChar()) + if (c.isLowerCase()) 1 else 27
         }
 
-        part2 {
+        val input = lines
 
+        part1(8088) {
+            input
+                .map { it.chunked(it.length/2) }
+                .map { (f, s) -> f.toSet() intersect s.toSet() }
+                .sumOf { it.priority() }
+        }
+
+        part2(2522) {
+            lines.chunked(3)
+                .map { (f, s, t) -> f.toSet() intersect s.toSet() intersect t.toSet() }
+                .sumOf { it.priority() }
         }
     }
 }
