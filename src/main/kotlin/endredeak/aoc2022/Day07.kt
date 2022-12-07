9573d4117841bea7b3ccebfc91acba2c9628360b
Endre Deak <deak@csdisco.com>
2022-12-06 22:14:59 -0800

    Day 07

--- src/main/kotlin/endredeak/aoc2022/Day07.kt
+++ src/main/kotlin/endredeak/aoc2022/Day07.kt
@@ -1,15 +1,55 @@
 package endredeak.aoc2022
 
+data class Dir(
+    val name: String,
+    var size: Long = 0,
+    val parent: Dir? = null,
+    val children: MutableList<Dir> = mutableListOf()
+) {
+    operator fun get(name: String) = this.children.first { it.name == name }
+
+    fun addSize(size: Long) {
+        var curr: Dir? = this
+        while (curr != null) {
+            curr.size += size
+            curr = curr.parent
+        }
+    }
+}
+
 fun main() {
-    solve( "") {
-        val input = lines
+    solve("No Space Left On Device") {
+        val input = run {
+            val root = Dir("/")
+            var curr = root
 
-        part1 {
+            lines
+                .drop(1)
+                .forEach { line ->
+                    when {
+                        line.contains("cd ..") -> curr = curr.parent ?: error("waat")
+                        line.contains("cd ") -> curr = curr[line.drop(5)]
+                        line.startsWith("dir ") -> curr.children.add(Dir(line.drop(4), parent = curr))
+                        line.first().isDigit() -> line.split(" ").first().let { curr.addSize(it.toLong()) }
+                        else -> {}
+                    }
+                }
 
+            val dirs = mutableListOf(Dir("/", root.size))
+            fun flatten(dir: Dir) {
+                dir.children
+                    .forEach {
+                        dirs.add(Dir(it.name, it.size))
+                        flatten(it)
+                    }
+            }
+            flatten(root)
+
+            dirs
         }
 
-        part2 {
+        part1(1770595) { input.filter { it.size <= 100000 }.sumOf { it.size } }
 
-        }
+        part2(2195372) { input.map { it.size }.sorted().first { it >= input.first().size - 40_000_000 } }
     }
 }
