7cf45cfe4bfc0f91bd00c75e6c75146fb41ff4be
Endre Deak <deak@csdisco.com>
2022-12-09 08:19:46 -0800

    Day 09 format fixed

--- src/main/kotlin/endredeak/aoc2022/Day09.kt
+++ src/main/kotlin/endredeak/aoc2022/Day09.kt
@@ -11,8 +11,10 @@ fun main() {
 
         operator fun Pair<Int, Int>.plus(p: Pair<Int, Int>) = this.first + p.first to this.second + p.second
         operator fun Pair<Int, Int>.minus(p: Pair<Int, Int>) = this.first - p.first to this.second - p.second
-        infix fun Pair<Int, Int>.follow(p: Pair<Int, Int>) = this.first + (p.first-this.first).sign to this.second + (p.second-this.second).sign
-        fun Pair<Int, Int>.notInRange() = listOf(this.first.absoluteValue, this.second.absoluteValue).any { it > 1}
+        fun Pair<Int, Int>.notInRange() = listOf(this.first.absoluteValue, this.second.absoluteValue).any { it > 1 }
+        infix fun Pair<Int, Int>.follow(p: Pair<Int, Int>) =
+            this.first + (p.first - this.first).sign to this.second + (p.second - this.second).sign
+
 
         fun move(dir: String) =
             when (dir) {
@@ -22,25 +24,27 @@ fun main() {
                 else -> 0 to -1
             }
 
-        fun simulateRope(n: Int = 2): Int {
+        fun simulateRope(n: Int = 2) = run {
             val knots = MutableList(n) { 0 to 0 }
             val path = mutableSetOf(0 to 0)
 
             input.forEach { (dir, dist) ->
                 repeat(dist) {
                     knots[0] += move(dir)
-                    repeat(knots.size-1) { i ->
+                    repeat(knots.size - 1) { i ->
                         val h = knots[i]
-                        val t = knots[i+1]
+                        val t = knots[i + 1]
 
-                        if ((h-t).notInRange()) { knots[i+1] = t follow h }
+                        if ((h - t).notInRange()) {
+                            knots[i + 1] = t follow h
+                        }
 
                         path += knots.last()
                     }
                 }
             }
 
-            return path.size
+            path.size
         }
 
         part1(6314) { simulateRope(2) }
