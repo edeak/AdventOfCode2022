0e40b2d50c2b570c242f921ec731988b5a189536
Endre Deak <deak@csdisco.com>
2022-12-09 23:01:04 -0800

    Day 10

--- src/main/kotlin/endredeak/aoc2022/Day10.kt
+++ src/main/kotlin/endredeak/aoc2022/Day10.kt
@@ -1,15 +1,40 @@
 package endredeak.aoc2022
 
 fun main() {
-    solve( "") {
-        val input = lines
-
-        part1 {
-
+    solve("Cathode-Ray Tube") {
+        val input = lines.map { l ->
+            if (l[0] == 'n') 1 to 0 else {
+                2 to l.substringAfter(" ").toInt()
+            }
         }
 
-        part2 {
+        var x = 1
+        var c = 0
 
+        fun run(func: () -> Unit) {
+            input.forEach { (w, v) ->
+                repeat(w) { _ -> c++; func() }
+                x += v
+            }
+        }
+
+        part1(13520) {
+            var s = 0
+            run {
+                if (c % 40 == 20) {
+                    s += c * x
+                }
+            }
+            s
+        }
+
+        part2("PGPHBEAB") {
+            x = 1
+            c = 0
+            run {
+                val char = if ((c % 40) - 1 in (x - 1..x + 1)) "#" else "."
+                if (c % 40 == 0) println(char) else print(char)
+            }
         }
     }
 }
