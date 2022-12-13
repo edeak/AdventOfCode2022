ec1fff7457f09c376f2be03b06830ddfbcf7038a
Endre Deak <deak@csdisco.com>
2022-12-13 07:27:55 -0800

    Day 13

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,5 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.21")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("com.google.code.gson:gson:2.10")
 }
