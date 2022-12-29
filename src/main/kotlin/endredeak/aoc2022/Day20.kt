8966caef4f380fd6c0e454f9bd75eb149dda3447
Endre Deak <deak@csdisco.com>
2022-12-29 15:20:02 -0800

    Day 20

--- src/main/kotlin/endredeak/aoc2022/Day20.kt
+++ src/main/kotlin/endredeak/aoc2022/Day20.kt
@@ -1,15 +1,34 @@
 package endredeak.aoc2022
 
 fun main() {
-    solve( "") {
-        val input = lines
+    solve("Grove Positioning System") {
+        fun input(key: Long? = null) = lines
+            .map { it.toLong() }
+            .mapIndexed { ix, i -> ix to (key?.let { it * i } ?: i) }
+            .toMutableList()
 
-        part1 {
-
+        fun MutableList<Pair<Int, Long>>.decrypt() {
+            indices.forEach { originalIdx ->
+                val idx = indexOfFirst { originalIdx == it.first }
+                val toMove = removeAt(idx)
+                add((idx + toMove.second).mod(size), toMove)
+            }
         }
 
-        part2 {
+        fun MutableList<Pair<Int, Long>>.grove() =
+            this.indexOfFirst { it.second == 0L }
+                .let { zero -> listOf(1000, 2000, 3000).sumOf { this[(zero + it) % this.size].second } }
 
+        part1(13967) {
+            input()
+                .apply { decrypt() }
+                .grove()
+        }
+
+        part2(1790365671518) {
+            input(811589153)
+                .apply { repeat(10) { decrypt() } }
+                .grove()
         }
     }
 }
